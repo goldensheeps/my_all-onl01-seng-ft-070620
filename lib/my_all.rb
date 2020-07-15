f19a237fd1f5bbde321a4a715d8af8d5dfd256a3
Timothy Lalli <trlalli92@gmail.com>
2020-07-15 19:29:00 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,8 +4,8 @@ def my_all?(array)
   i = 0 
   block_return_values = []
   while i < array.length
-    block_return_values <<
-  yield(array[i])
+    block_return_values << yield(array[i])
     i += 1 
   end
+  block_return_values
 end
